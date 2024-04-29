0e9baf7dec85259d07db145f2c840fb4ac244659
Martina Bujasová <martina.bujasova2@gmail.com>
2024-04-29 14:35:44 +0200

    Fix: ukol2 komponenty hotovo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,14 @@ import { Banner } from '../components/banner/Banner';
 import { Gallery } from '../components/gallery/Gallery';
 import { Contact } from '../components/contact/Contact';
 import { Footer } from '../components/footer/Footer';
+import { Menu } from '../components/menu/Menu';
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
     <main>
       <Banner />
+      <Menu />
       <Gallery />
       <Contact />
     </main>
