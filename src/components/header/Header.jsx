afad2ccb70ee49d298527d95ba2f2c1de8e1dd1c
Martina Bujasová <martina.bujasova2@gmail.com>
2024-04-30 12:29:24 +0200

    Add: úkol3 Stránka pro detail objednávky

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,35 @@
 import './Header.css';
 
-export const Header = () => {
-  return (
-    <header>
-      <div className="header__content container">
-        <div className="site-logo"></div>
+export const Header = ({ showMenu }) => {
+  if (showMenu === true) {
+    return (
+      <header>
+        <div className="header__content container">
+          <div className="site-logo"></div>
 
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
+          <div className="navigation">
+            <button className="nav-btn"></button>
+            <nav className="rollout-nav nav-closed">
+              <a href="#home">domů</a>
+              <a href="#menu">menu</a>
+              <a href="#gallery">galerie</a>
+              <a href="#contact">kontakt</a>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <div className="header__content container">
+          <div className="site-logo"></div>
+
+          <nav className="inline-nav">
+            <a href="/">Hlavní stránka</a>
           </nav>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 };
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,20 +8,24 @@ export const Menu = ({ drinks }) => {
         <p className="menu-intro">
           Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
         </p>
-      </div>
-      <div className="drinks-list">
-        {drinks.map((drink) => {
-          return (
-            <Drink
-              key={drink.id}
-              id={drink.id}
-              name={drink.name}
-              ordered={drink.ordered}
-              image={drink.image}
-              layers={drink.layers}
-            />
-          );
-        })}
+
+        <div className="drinks-list">
+          {drinks.map((drink) => {
+            return (
+              <Drink
+                key={drink.id}
+                id={drink.id}
+                name={drink.name}
+                ordered={drink.ordered}
+                image={drink.image}
+                layers={drink.layers}
+              />
+            );
+          })}
+        </div>
+        <div className="order-detail">
+          <a href="/order.html">Detail objednávky</a>
+        </div>
       </div>
     </section>
   );
